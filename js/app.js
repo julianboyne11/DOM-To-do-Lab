db2d1e76a2c9ccf3ba49d0b3187b37cae484d157
julianboyne <julianboyne11@gmail.com>
2022-08-30 21:46:08 -0400

    finish lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("sanity check");
+const input = document.querySelector("#list") 
+const btn = document.querySelector("#submit-button")
+const ulElement = document.querySelector("todo-list")
+
+// ulElement.addEventListener("click", handleClick)
+
+
+btn.addEventListener("click", function(evt) {
+  
+  const li = document.createElement("li")
+  // console.log(liElement)
+  // set new <li> to <input>
+  li.textContent = input.value
+  // console.dir(li)
+  //rester the input
+  input.value = ""
+  //add the text value into the <ul>
+  document.querySelector("ul").appendChild(li)
+})
